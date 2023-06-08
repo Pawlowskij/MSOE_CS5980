4b54f6dba3e8eeead23addc780b866956a99dadf
SweatyGymSock <justin.pawlowski22@gmail.com>
2023-06-07 21:29:11 -0500

    first commit

---
+++ Week1/pawlowskijl1.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+#Load in .json file as Dataframe
+df = pd.read_json("autos.json")
+
+#Function to calculate fuel economy
+
+def CalcFuelEcon(carSelection, df, percentCity):
+    pass
+    pass
+#Print small summary of DF
+print(df[["make","drive-wheels", "city-mpg", "highway-mpg"]].to_markdown())
+
+#Print total cars in DF
+totalCars = len(df.index)-1
+print(f'\nTotal number of cars: {totalCars}')
+
+#Ask for integer value within the bounds of the DF
+while True:
+    carSelection = input("\nPlease select a vehicle index: ")
+    
+    if carSelection.isdigit() and int(carSelection) <= totalCars:
+        carSelection = int(carSelection)
+        break
+
+#Print all details of input car index    
+print(df.iloc[carSelection])
+
+
+
